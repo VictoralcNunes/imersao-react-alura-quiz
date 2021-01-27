5142bdf4d7324200573b25229347b9717e69756e
victoralcnunes <victoralcnunes@hotmail.com>
2021-01-27 00:47:22 -0300

    Second part/class (Linter, Routes, State)

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,14 @@
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
 import db from '../db.json';
 import Widget from '../src/components/Widget';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
 import QuizBackground from '../src/components/QuizBackground';
 import QuizLogo from '../src/components/QuizLogo';
-
-// const BackgroundImage = styled.div`
-//   background-image: url(${db.bg});
-//   flex: 1;
-//   background-size: cover;
-//   background-position: center;
-// `;
 
 export const QuizContainer = styled.div`
   width:100%;
@@ -24,29 +21,55 @@ export const QuizContainer = styled.div`
   }
 `;
 
-
 export default function Home() {
+  const router = useRouter();
+  const [name, setName] = React.useState('');
+
   return (
-      <QuizBackground backgroundImage={db.bg}>
-        <QuizContainer>
-          <Widget>
-            <Widget.Header>
-              <h1>The legend of zelda</h1>
-            </Widget.Header>
-            <Widget.Content>
-              <p>lorem ipseum dolor sit amet...</p>
-            </Widget.Content>
-          </Widget>
-          <Widget>
-              
-            <Widget.Content>
-              <h1>Quizes da galera</h1>
-              <p>lorem ipseum dolor sit amet...</p>
-            </Widget.Content>
-          </Widget>
-          <Footer />
-        </QuizContainer>
-        <GitHubCorner projectUrl={"https://www.github.com/victoralcnunes"}/>
-      </QuizBackground>
-    );
-  }
+    <QuizBackground backgroundImage={db.bg}>
+      <QuizContainer>
+        <Head>
+          <title>Alura Quiz!</title>
+        </Head>
+        <QuizLogo />
+        <Widget>
+          <Widget.Header>
+            <h1>The legend of zelda</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <p>lorem ipseum dolor sit amet...</p>
+          </Widget.Content>
+          <Widget.Content>
+            <form onSubmit={function (infosDoEvento) {
+              infosDoEvento.preventDefault();
+              router.push(`/quiz?name=${name}`);
+              // eslint-disable-next-line no-console
+              console.log(`Submissão de nome e ínicio do quiz ${name}`);
+            }}
+            >
+              <input
+                onChange={function (infosDoEvento) {
+                  setName(infosDoEvento.target.value);
+                }}
+                placeholder="Diz pra nós seu nome!"
+              />
+              <button type="submit" disabled={name.length === 0}>
+                Jogar
+                {name}
+              </button>
+            </form>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+
+          <Widget.Content>
+            <h1>Quizes da galera</h1>
+            <p>lorem ipseum dolor sit amet...</p>
+          </Widget.Content>
+        </Widget>
+        <Footer />
+      </QuizContainer>
+      <GitHubCorner projectUrl="https://www.github.com/victoralcnunes/imersao-react-alura-quiz" />
+    </QuizBackground>
+  );
+}
